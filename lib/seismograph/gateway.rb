66ee17f7d544a13a9a5c2da0ea827e9a9796a524
Burke Webster <burke.webster@gmail.com>
2015-09-18 09:18:14 -0600

    Fix for nil env

--- lib/seismograph/gateway.rb
+++ lib/seismograph/gateway.rb
@@ -14,7 +14,14 @@ module Seismograph
       def client
         @client ||= Statsd.new(Seismograph.config.statsd_host,
                                Seismograph.config.statsd_port,
-                               namespace: [Seismograph.config.app_name, Seismograph.config.env].join('.'))
+                               namespace: namespace)
+      end
+
+      def namespace
+        [
+          Seismograph.config.app_name,
+          Seismograph.config.env
+        ].compact.join('.')
       end
     end
   end
