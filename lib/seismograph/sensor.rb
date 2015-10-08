761994cd0799e87c56e70bd9205a4c453430a15b
mhuggins <matt.huggins@gmail.com>
2015-10-08 07:51:54 -0600

    Track success and failure for benchmark and count

--- lib/seismograph/sensor.rb
+++ lib/seismograph/sensor.rb
@@ -19,22 +19,31 @@ module Seismograph
       Gateway.increment(stat(description), gateway_params(params))
     end
 
-    def benchmark(description, params = {})
-      Gateway.time(stat(description), gateway_params(params)) do
-        yield
+    def decrement(description, params = {})
+      Gateway.decrement(stat(description), gateway_params(params))
+    end
+
+    def benchmark(description, params = {}, &block)
+      with_success_and_failure(description, params) do
+        Gateway.time(stat(description), gateway_params(params), &block)
       end
     end
 
     private
 
-    def track(description, amount, params = {}, &block)
-      begin
-        block.call if block_given?
+    def track(description, amount, params = {})
+      with_success_and_failure(description, params) do
+        yield if block_given?
         Gateway.histogram(stat(description), amount, gateway_params(params))
-      rescue StandardError => e
-        increment("#{description}.failure", gateway_params(params))
-        raise e
       end
+    end
+
+    def with_success_and_failure(description, params)
+      yield
+      increment("#{description}.success", gateway_params(params))
+    rescue StandardError => e
+      increment("#{description}.failure", gateway_params(params))
+      raise e
     end
 
     def stat(description)
--- spec/seismograph/sensor_spec.rb
+++ spec/seismograph/sensor_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 RSpec.describe Seismograph::Sensor do
   subject { described_class.new('mynamespace') }
 
-  let(:client_double) { double('statsd client', histogram: true, increment: true, time: true) }
+  let(:client_double) { double('statsd client', histogram: true, increment: true, decrement: true, time: true) }
 
   before do
     allow(Seismograph::Gateway).to receive(:client).and_return(client_double)
@@ -11,6 +11,20 @@ RSpec.describe Seismograph::Sensor do
 
     Seismograph.config do |config|
       config.app_name = 'myapp'
+    end
+  end
+
+  describe '#increment' do
+    it 'accepts tags' do
+      subject.increment('metric', tags: 'sometag')
+      expect(client_double).to have_received(:increment).once.with('mynamespace.metric', tags: %w[sometag app:myapp])
+    end
+  end
+
+  describe '#decrement' do
+    it 'accepts tags' do
+      subject.decrement('metric', tags: 'sometag')
+      expect(client_double).to have_received(:decrement).once.with('mynamespace.metric', tags: %w[sometag app:myapp])
     end
   end
 
@@ -27,44 +41,82 @@ RSpec.describe Seismograph::Sensor do
 
     it 'accepts a block' do
       x = 0
-      subject.count('metric', 2) do
-        x = 1
+      subject.count('metric', 2) { x = 1 }
+      expect(x).to eql(1)
+    end
+
+    describe 'when an error is not raised' do
+      def count
+        subject.count('metric', 2) { }
       end
 
-      expect(x).to eql(1)
-      expect(client_double).to have_received(:histogram).once.with('mynamespace.metric', 2, tags: %w[app:myapp])
+      it 'tracks histogram' do
+        count
+        expect(client_double).to have_received(:histogram).once.with('mynamespace.metric', 2, tags: %w[app:myapp])
+      end
+
+      it 'increments successes' do
+        count
+        expect(client_double).to have_received(:increment).once.with('mynamespace.metric.success', tags: %w[app:myapp])
+      end
     end
 
-    it 'increments failures if exception is thrown' do
-      subject.count('metric', 1) do
-        fail 'My error'
-      end rescue nil
+    describe 'when an error is raised' do
+      def count
+        subject.count('metric', 1) { fail 'My error' }
+      end
 
-      expect(client_double).to have_received(:increment).once.with('mynamespace.metric.failure', tags: %w[app:myapp])
-    end
+      it 'does not track histogram' do
+        count rescue nil
+        expect(client_double).to_not have_received(:histogram)
+      end
 
-    it 're-raises error' do
-      expect {
-        subject.count('metric', 4) do
-          fail 'My error'
-        end
-      }.to raise_error(RuntimeError)
-    end
-  end
+      it 'increments failures' do
+        count rescue nil
+        expect(client_double).to have_received(:increment).once.with('mynamespace.metric.failure', tags: %w[app:myapp])
+      end
 
-  describe '#increment' do
-    it 'accepts tags' do
-      subject.increment('metric', tags: 'sometag')
-      expect(client_double).to have_received(:increment).once.with('mynamespace.metric', tags: %w[sometag app:myapp])
+      it 're-raises error' do
+        expect { count }.to raise_error(RuntimeError)
+      end
     end
   end
 
   describe '#benchmark' do
-    it 'benchmark time' do
-      subject.benchmark('metric') do
-        # Slow stuff
+    describe 'when an error is not raised' do
+      def benchmark
+        subject.benchmark('metric') { }
       end
-      expect(client_double).to have_received(:time).once.with('mynamespace.metric', tags: %w[app:myapp])
+
+      it 'tracks benchmark time' do
+        benchmark
+        expect(client_double).to have_received(:time).once.with('mynamespace.metric', tags: %w[app:myapp])
+      end
+
+      it 'increments successes' do
+        benchmark
+        expect(client_double).to have_received(:increment).once.with('mynamespace.metric.success', tags: %w[app:myapp])
+      end
+    end
+
+    describe 'when an error is raised' do
+      def benchmark
+        subject.benchmark('metric') { fail 'My error' }
+      end
+
+      before do
+        allow(client_double).to receive(:time).and_raise(RuntimeError, 'My error')
+      end
+
+      it 'tracks benchmark time' do
+        benchmark rescue nil
+        expect(client_double).to have_received(:time).once.with('mynamespace.metric', tags: %w[app:myapp])
+      end
+
+      it 'increments failures' do
+        benchmark rescue nil
+        expect(client_double).to have_received(:increment).once.with('mynamespace.metric.failure', tags: %w[app:myapp])
+      end
     end
   end
 end
